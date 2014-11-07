ac4453a10a1a22e99ad0df452598edb37ca9fa68
Ståle Tomten <stale.tomten@broadnet.no>
2014-11-07 18:31:33 +0100

    Reformat.

--- rss-notify/src/main/java/org/tomten/notify/rss/HawtioConfiguration.java
+++ rss-notify/src/main/java/org/tomten/notify/rss/HawtioConfiguration.java
@@ -1,16 +1,8 @@
 package org.tomten.notify.rss;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.hawt.config.ConfigFacade;
 import io.hawt.springboot.HawtPlugin;
 import io.hawt.springboot.PluginService;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,15 +14,17 @@ public class HawtioConfiguration {
 
     /**
      * Loading an example plugin
+     *
      * @return
      */
     @Bean
     public HawtPlugin samplePlugin() {
-        return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[] { "sample-plugin/js/sample-plugin.js" });
+        return new HawtPlugin("sample-plugin", "/hawtio/plugins", "", new String[]{ "sample-plugin/js/sample-plugin.js" });
     }
 
     /**
      * Set things up to be in offline mode
+     *
      * @return
      * @throws Exception
      */
@@ -47,10 +41,11 @@ public class HawtioConfiguration {
 
     /**
      * Register rest endpoint to handle requests for /plugin, and return all registered plugins.
+     *
      * @return
      */
     @Bean
-    public PluginService pluginService(){
+    public PluginService pluginService() {
         return new PluginService();
     }
 
