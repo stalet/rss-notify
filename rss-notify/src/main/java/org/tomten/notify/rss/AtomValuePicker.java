531bf78a1bcaf7f0418be17ffa9900ded2233744
Ståle Tomten <Stale.Tomten@gmail.com>
2014-11-17 08:37:08 +0100

    Broker that sends notifications to irc.

--- rss-notify/src/main/java/org/tomten/notify/rss/AtomValuePicker.java
+++ rss-notify/src/main/java/org/tomten/notify/rss/AtomValuePicker.java
@@ -11,8 +11,9 @@ public class AtomValuePicker implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         Entry entry = exchange.getIn().getBody(Entry.class);
-        exchange.setOut(exchange.getIn());
         exchange.getOut().setHeader("title", entry.getTitle());
-        exchange.getOut().setHeader("description", entry.getUpdated());
+        final String description = "Jenkins build FAILED: " + entry.getTitle() + "(" + entry.getLink("alternate").getHref() + ")";
+        exchange.getOut().setHeader("description", description);
+        exchange.getOut().setBody(description);
     }
 }
--- rss-notify/src/main/java/org/tomten/notify/rss/CamelConfiguration.java
+++ rss-notify/src/main/java/org/tomten/notify/rss/CamelConfiguration.java
@@ -2,23 +2,34 @@ package org.tomten.notify.rss;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.atom.AtomConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Configuration
 public class CamelConfiguration {
+
+    private final static DateFormat FORMAT = new SimpleDateFormat(AtomConverter.DATE_PATTERN_NO_TIMEZONE);
+
+    private final static String IRC_USERNAME = "JenkinsBoT1000";
+    private final static String IRC_HOST = "irc.serv.as2116.net:6667";
+    private final static String IRC_CHANNEL = "#bss";
 
     @Bean
     RouteBuilder jenkinsRouteBuilder() {
         return new RouteBuilder() {
             @Override
             public void configure() throws Exception {
-                String rssURL = "https://d1jenkins01.dev.as2116.net/rssAll";
+                String rssURL = "http://d1jenkins01.dev.as2116.net:8080/rssFailed";
 
-                from("atom:" + rssURL).
+                from("atom:" + rssURL + "?lastUpdate=" + FORMAT.format(new Date())).
                         to("bean:atomValuePicker").
-                        to("bean:notifySendProcessor");
+                        to("irc:" + IRC_USERNAME + "@" + IRC_HOST + "/" + IRC_CHANNEL);
 
             }
         };
