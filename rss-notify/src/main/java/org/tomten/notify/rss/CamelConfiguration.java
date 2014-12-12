b09aa37885b0ca8f32085489f71e9f0bc0593a3c
Ståle Tomten <staale@tomten.org>
2014-12-12 11:06:44 +0100

    Project version bump.

--- rss-notify/src/main/java/org/tomten/notify/rss/CamelConfiguration.java
+++ rss-notify/src/main/java/org/tomten/notify/rss/CamelConfiguration.java
@@ -14,28 +14,6 @@ import java.util.Date;
 @Configuration
 public class CamelConfiguration {
 
-    private final static DateFormat FORMAT = new SimpleDateFormat(AtomConverter.DATE_PATTERN_NO_TIMEZONE);
-
-    private final static String IRC_USERNAME = "JenkinsBoT1000";
-    private final static String IRC_HOST = "irc.serv.as2116.net:6667";
-    private final static String IRC_CHANNEL = "#bss";
-
-    @Bean
-    RouteBuilder jenkinsRouteBuilder() {
-        return new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                String rssURL = "http://d1jenkins01.dev.as2116.net:8080/rssFailed";
-
-                from("atom:" + rssURL + "?lastUpdate=" + FORMAT.format(new Date())).
-                        to("bean:atomValuePicker").
-                        to("irc:" + IRC_USERNAME + "@" + IRC_HOST + "/" + IRC_CHANNEL);
-
-            }
-        };
-    }
-/*
-
     @Bean
     RouteBuilder rssRouteBuilder() {
         return new RouteBuilder() {
@@ -52,7 +30,6 @@ public class CamelConfiguration {
             }
         };
     }
-*/
     // Collaborators fixtures
     @Autowired
     CamelContext camelContext;
